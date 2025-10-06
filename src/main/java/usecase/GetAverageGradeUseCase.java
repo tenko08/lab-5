9cd6391859d9a1a233e41094d65b04a8dec2c8a5
tenko08 <unalai1027@gmail.com>
2025-10-06 13:59:09 -0400

    average grade logic

--- src/main/java/usecase/GetAverageGradeUseCase.java
+++ src/main/java/usecase/GetAverageGradeUseCase.java
@@ -1,7 +1,6 @@
 package usecase;
 
 import api.GradeDataBase;
-import entity.Grade;
 import entity.Team;
 
 /**
@@ -28,6 +27,10 @@ public final class GetAverageGradeUseCase {
         // Call the API to get all the grades for the course for all your team members
         // TODO Task 3a: Complete the logic of calculating the average course grade for
         //              your team members. Hint: the getGrades method might be useful.
+        for (String username : team.getMembers()) {
+            sum += gradeDataBase.getGrade(username, course).getGrade();
+            count++;
+        }
 
         if (count == 0) {
             return 0;
